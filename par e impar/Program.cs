eedcd0c5288edd3e221beec7ad12545e99862686
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 02:09:15 -0600

    Agregar archivos de proyecto.

---
+++ par e impar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace par_e_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Numero1;
+            double respuesta;
+
+
+
+            Console.WriteLine("Ingresa el numero:");
+            Numero1 = Convert.ToDouble(Console.ReadLine());
+
+            respuesta = Numero1 % 2;
+
+            if (respuesta == 0)
+            {
+                Console.WriteLine("El numero es par.");
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar.");
+            }
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
